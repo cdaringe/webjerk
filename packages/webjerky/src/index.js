2a58a3d11aae391c5772d181527212df4e4a58ce
cdaringe <christopher.dieringer@gmail.com>
2017-04-06 11:41:39 -0700

    fix(webjerky): wdio-screenshot + ff only
    Browser desiredCapabilities combos are sensitive!

--- packages/webjerky/src/index.js
+++ packages/webjerky/src/index.js
@@ -2,6 +2,7 @@
 
 var path = require('path')
 var wj = require('webjerk')
+var getSnapsConfig = require('./snaps-conf')
 
 /**
  * @module webjerky
@@ -32,72 +33,14 @@ module.exports = function (conf) {
         register: require('webjerk-process-wrangler'),
         config: {
           cp: {
-            // yes, this bin lookup is fragile!
-            bin: path.resolve(__dirname, '..', 'node_modules', '.bin', 'httpster'),
+            bin: path.resolve(__dirname, '..', 'node_modules', '.bin', 'httpster'), // yes, this bin lookup is fragile!
             args: ['-d', conf.staticDir]
           }
         }
       },
       {
         register: require('webjerk-snaps'),
-        config: {
-          concurrency: 5,
-          desiredCapabilities: [
-            // {
-            //   browserName: 'firefox',
-            //   tags: ['webjerk-ff'],
-            //   name: conf.testName
-            // },
-            {
-              browserName: 'chrome',
-              platform: 'macOS 10.12',
-              version: '57.0',
-              tags: ['webjerk-chrome'],
-              name: conf.testName
-            }
-            // ,
-            // {
-            //   browserName: 'safari',
-            //   tags: ['webjerk-safari'],
-            //   name: conf.testName
-            // }
-            // ,
-            // {
-            //   browserName: 'MicrosoftEdge',
-            //   platform: 'Windows 10',
-            //   tags: ['webjerk-edge'],
-            //   name: conf.testName
-            // },
-            // {
-            //   browserName: 'internet explorer',
-            //   platform: 'Windows 10',
-            //   version: '11.103',
-            //   tags: ['webjerk-ie'],
-            //   name: conf.testName
-            // }
-          ],
-          port: 4445,
-          webdriverio: {
-            services: ['sauce'],
-            user: process.env.SAUCE_USERNAME,
-            key: process.env.SAUCE_ACCESS_KEY
-            // https://github.com/webdriverio/webdriverio/issues/1683#issue-186968231
-            // further, the below options don't seem to be honored, which is a bummer.
-            // that's fine, we can open a tunnel independently
-            // sauceConnect: true,
-            // sauceConnectOpts: {
-            //   verbose: true,
-            //   verboseDebugging: true,
-            //   username: process.env.SAUCE_USERNAME,
-            //   accessKey: process.env.SAUCE_ACCESS_KEY,
-            //   tunnelIdentifier: `webjerk-${conf.testName}`
-            // }
-          },
-          url: 'http://localhost:3333', // httpster by default serves on 3333
-          testName: conf.testName || 'webjerk-test',
-          snapDefinitions: conf.snapDefinitions,
-          snapDefinitionsFromWindow: conf.snapDefinitionsFromWindow
-        }
+        config: getSnapsConfig(conf)
       }
     ]
   }

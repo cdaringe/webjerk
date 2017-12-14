b82197a570330f39ce71d4fa2885062ffed2d7fc
cdaringe <christopher.dieringer@gmail.com>
2017-12-13 22:39:27 -0800

    fix(reporter): de-biff ui build

--- packages/webjerk-image-set-diff-reporter/index.js
+++ packages/webjerk-image-set-diff-reporter/index.js
@@ -7,6 +7,7 @@ var debug = require('debug')('webjerk:image-set-diff-reporter')
 
 module.exports = async function webjerkImageSetDiffReporter ({ differences, dest }) {
   if (!dest) throw new Error('missing dest')
+  var localPackageJson = path.resolve(__dirname, 'package.json')
   debug(`building report`)
   var diffBasename = `differences-${Date.now()}.json`
   var serializedDiffs = JSON.stringify(differences, null, 2)
@@ -16,6 +17,11 @@ module.exports = async function webjerkImageSetDiffReporter ({ differences, dest
     fs.remove(dest)
   ])
   var env = Object.assign({ REACT_APP_DIFFS: diffBasename, NODE_ENV: 'production' })
+  debug('protecting build characterstics')
+  var pkg = JSON.parse((await fs.readFile(localPackageJson)).toString())
+  delete pkg.repository
+  delete pkg.homepage
+  await fs.writeFile(localPackageJson, JSON.stringify(pkg, null, 2))
   debug('running react-scripts build')
   await execa('npm', ['run', 'build'], { cwd: __dirname, env, stdio: 'inherit' })
   await fs.move(path.join(__dirname, 'build'), dest)
